59cfbc2f67c9413a87704e59f2966ee5c4b8907d
yinm <yinm.cminor9@gmail.com>
2017-10-31 23:30:23 +0900

    Add 2-5-6

--- ch2/01.rb
+++ ch2/01.rb
@@ -1,9 +1,16 @@
-class Ruler
-  attr_accessor :length
+class MyClass
+  @@cvar = 'Hello, My class variable!'
 
-  def self.pair
-    [new, new]
+  def cvar_in_method
+    puts @@cvar
+  end
+
+  def self.cvar_in_class_method
+    puts @@cvar
   end
 end
 
-p Ruler.pair
+my_object = MyClass.new
+
+my_object.cvar_in_method
+MyClass.cvar_in_class_method
