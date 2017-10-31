4ec43eca0a6019a596ef72eedf5bf60cbb44eb74
yinm <yinm.cminor9@gmail.com>
2017-10-31 23:32:12 +0900

    Add 2-5-6

--- ch2/01.rb
+++ ch2/01.rb
@@ -1,16 +1,16 @@
 class MyClass
-  @@cvar = 'Hello, My class variable!'
+  @@class_var = 'Hello, My class variable'
 
-  def cvar_in_method
-    puts @@cvar
+  def class_var_in_method
+    puts @@class_var
   end
 
-  def self.cvar_in_class_method
-    puts @@cvar
+  def self.class_var_in_class_method
+    puts @@class_var
   end
 end
 
 my_object = MyClass.new
 
-my_object.cvar_in_method
-MyClass.cvar_in_class_method
+my_object.class_var_in_method
+MyClass.class_var_in_class_method
