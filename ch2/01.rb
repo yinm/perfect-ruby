6bd142859b06533d7a26d5a74a39ba2198cec082
yinm <yinm.cminor9@gmail.com>
2017-11-01 07:37:38 +0900

    Add 2-7-5

--- ch2/01.rb
+++ ch2/01.rb
@@ -1,11 +1,6 @@
 colors = {
-    'red' => 'ff0000',
-    'green' => '00ff00',
-    'blue' => '0000ff'
+    :red => 'ff0000',
+    :green => '00ff00',
+    :blue => '0000ff'
 }
-
-p colors['blue']
-p colors['black']
-
-colors['green'] = '008000'
-p colors
+p colors[:blue]
