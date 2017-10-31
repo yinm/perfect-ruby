4922db1e1086d9633f58198b948ef1f858f053a9
yinm <yinm.cminor9@gmail.com>
2017-10-31 23:26:34 +0900

    Add 2-5-5

--- ch2/01.rb
+++ ch2/01.rb
@@ -1,10 +1,9 @@
 class Ruler
   attr_accessor :length
 
-  def initialize(length)
-    @length = length
+  def self.pair
+    [Ruler.new, Ruler.new]
   end
 end
 
-ruler = Ruler.new(30)
-puts ruler.length
+p Ruler.pair
