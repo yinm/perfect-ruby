b96ef91b35a0c8afcf118db29c67b1932cb5d5b3
yinm <yinm.cminor9@gmail.com>
2017-11-02 08:30:12 +0900

    Add 3-2-3

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,3 +1,10 @@
-2.times do
-  puts 'こんにちは！'
+languages = %w(Perl Python Ruby Smalltalk JavaScript)
+
+languages.each do |language|
+  puts language
+
+  if language == 'Ruby'
+    puts 'I found Ruby!'
+    break
+  end
 end
