c7f826b76310aaa8855fa6c3e521bedfdab9a5b4
yinm <yinm.cminor9@gmail.com>
2017-11-02 08:35:37 +0900

    Add 3-2-3

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,10 +1,13 @@
-languages = %w(Perl Python Ruby Smalltalk JavaScript)
+def jump_state
+  languages = %w(Perl Python Ruby Smalltalk JavaScript)
+  languages.each do |language|
+    puts language
 
-languages.each do |language|
-  puts language
-
-  if language == 'Ruby'
-    puts 'I found Ruby!'
-    break
+    if language == 'Ruby'
+      puts 'I found Ruby!'
+      break 'I found Ruby!'
+    end
   end
 end
+
+p jump_state
