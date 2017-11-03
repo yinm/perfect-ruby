c4f4048878e4bf7bc88db73a2ee69937d90948b7
yinm <yinm.cminor9@gmail.com>
2017-11-03 17:11:16 +0900

    Add 3-5-3

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,8 +1,10 @@
-sweet = 'honey'
+people = %w(alice bob carol)
 
-def sweet
-  'salt'
-end
+person_0th = people[0]
+person_9th = people[9]
 
-p sweet
-p sweet()
+p person_0th&.capitalize
+p person_0th.capitalize
+
+p person_9th&.capitalize
+p person_9th.capitalize
