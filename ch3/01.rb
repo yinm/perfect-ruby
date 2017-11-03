b91bc4acd91134bc06804a339572ea3423447fdb
yinm <yinm.cminor9@gmail.com>
2017-11-03 17:03:39 +0900

    Add 3-4-1

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,15 +1,12 @@
-catch :triple_loop do
-  loop do
-    puts 'one'
-
-    loop do
-      puts 'two'
-
-      loop do
-        puts 'three'
-
-        throw :triple_loop
-      end
+catch :outer do
+  catch :middle do
+    catch :inner do
+      throw :middle
+      puts 'この行は実行されない'
     end
+
+    puts 'この行も実行されない'
   end
+
+  puts 'この行は実行する'
 end
