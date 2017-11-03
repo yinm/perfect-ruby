51a8701cef85f35572d3560d84ed1a0779c07ce3
yinm <yinm.cminor9@gmail.com>
2017-11-03 17:05:24 +0900

    Add 3-4-2

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,12 +1,3 @@
-catch :outer do
-  catch :middle do
-    catch :inner do
-      throw :middle
-      puts 'この行は実行されない'
-    end
-
-    puts 'この行も実行されない'
-  end
-
-  puts 'この行は実行する'
-end
+p catch(:foo) {
+  throw :foo, 'returned value'
+}
