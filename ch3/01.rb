cf667abcc221fe390f85eb516cc3a86d58a574c1
yinm <yinm.cminor9@gmail.com>
2017-11-03 17:48:11 +0900

    Add 3-5-9

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,11 +1,4 @@
-def block_sample(&block)
-  puts 'stand up'
+people = %w(Alice Bob Charlie)
+block = Proc.new {|name| puts name}
 
-  block.call if block
-
-  puts 'sit down'
-end
-
-block_sample do
-  puts 'walk'
-end
+people.each &block
