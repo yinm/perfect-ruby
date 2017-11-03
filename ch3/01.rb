75a8f114612a3f57068eb5cee6184d6375bb99ec
yinm <yinm.cminor9@gmail.com>
2017-11-03 17:12:33 +0900

    Add 3-5-4

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,10 +1,5 @@
-people = %w(alice bob carol)
+def Hello
+  puts 'Hello, My method!'
+end
 
-person_0th = people[0]
-person_9th = people[9]
-
-p person_0th&.capitalize
-p person_0th.capitalize
-
-p person_9th&.capitalize
-p person_9th.capitalize
+Hello()
