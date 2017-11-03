516d3b2c46d5c4ca2c882aa9ead9ed73928ea1e1
yinm <yinm.cminor9@gmail.com>
2017-11-03 16:42:20 +0900

    Add 3-2-3

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,5 +1,7 @@
 begin
-  raise StandardError
-rescue => e
-  puts "Error occurred (#{e.class}"
+  raise ArgumentError
+rescue LoadError => e
+  puts "1つ目の例外 #{e.class}"
+rescue ArgumentError => e
+  puts "2つ目の例外 #{e.class}"
 end
