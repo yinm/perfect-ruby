eea9d82d2d79f128d0cd39d2952357e83f1fbbdb
yinm <yinm.cminor9@gmail.com>
2017-11-03 17:28:31 +0900

    Add 3-5-9

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,8 +1,3 @@
-def greet_twice(name, first_message, second_message)
-  puts "#{first_message}, #{name}."
-  puts "#{second_message}, #{name}."
+%w(Alice Bob Charlie).each do |name|
+  puts "Hello, #{name}."
 end
-
-greetings = %w(Hello Hola)
-
-greet_twice('Ruby', *greetings)
