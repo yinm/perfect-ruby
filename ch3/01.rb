e8452fbf6f0e94df4eef3620aa3fe18e9f573023
yinm <yinm.cminor9@gmail.com>
2017-11-04 18:08:40 +0900

    Add 3-6-2

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,9 +1,1 @@
-ENV['HOGE'] = 'huga'
-
-system('echo $HOGE')
-system({'HOGE' => 'piyo'}, 'echo $HOGE')
-
-pid = spawn({'HOGE' => 'bar'}, 'echo $HOGE')
-Process.waitpid pid
-
-exec({'HOGE' => 'baz'}, 'echo $HOGE')
+system('echo `pwd`', chdir: '/tmp')
