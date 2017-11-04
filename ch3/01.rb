2d817ae61e1bf7ed3902539e45546a767dd8361e
yinm <yinm.cminor9@gmail.com>
2017-11-04 17:39:11 +0900

    Add 3-5-10

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,5 +1,8 @@
-def keywords(hash = {})
-  hash
+def keywords_with_options(alice: nil, bob: nil, **others)
+  {alice: alice, bob: bob, others: others}
 end
 
-p keywords(alice: 'アリス', bob: 'ボブ')
+
+p keywords_with_options(alice: 'アリス', bob: 'ボブ', charlie: 'チャーリィ', dave: 'デイヴ')
+p keywords_with_options(charlie: 'チャーリィ', alice: 'アリス', bob: 'ボブ')
+p keywords_with_options(alice: 'アリス', bob: 'ボブ')
