9f2a7c8bf7b953593751d361257456e64f718559
yinm <yinm.cminor9@gmail.com>
2017-11-04 17:40:27 +0900

    Add 3-5-12

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,8 +1,1 @@
-def keywords_with_options(alice: nil, bob: nil, **others)
-  {alice: alice, bob: bob, others: others}
-end
-
-
-p keywords_with_options(alice: 'アリス', bob: 'ボブ', charlie: 'チャーリィ', dave: 'デイヴ')
-p keywords_with_options(charlie: 'チャーリィ', alice: 'アリス', bob: 'ボブ')
-p keywords_with_options(alice: 'アリス', bob: 'ボブ')
+p Time::now
