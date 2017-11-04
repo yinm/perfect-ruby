e28f8d6b5fb651e7d2c1ecb062582ef5fc246982
yinm <yinm.cminor9@gmail.com>
2017-11-04 18:08:06 +0900

    Add 3-6-2

--- ch3/01.rb
+++ ch3/01.rb
@@ -1,2 +1,9 @@
-pid = spawn('uname')
+ENV['HOGE'] = 'huga'
+
+system('echo $HOGE')
+system({'HOGE' => 'piyo'}, 'echo $HOGE')
+
+pid = spawn({'HOGE' => 'bar'}, 'echo $HOGE')
 Process.waitpid pid
+
+exec({'HOGE' => 'baz'}, 'echo $HOGE')
