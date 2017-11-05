2bea44a7174e4edd2ca4e08860cc65afb379f6d0
yinm <yinm.cminor9@gmail.com>
2017-11-06 07:41:03 +0900

    Add 4-1-5

--- ch4/01.rb
+++ ch4/01.rb
@@ -1,16 +1,18 @@
-class Ruler
-  attr_accessor :length
-
-  class << self
-    def pair
-      [new, new]
-    end
-
-    def trio
-      [new, new ,new]
-    end
+class Processor
+  def process
+    protected_process
   end
+
+  def protected_process
+    private_process
+  end
+  protected :protected_process
+
+  def private_process
+    puts 'done!'
+  end
+  private :private_process
 end
 
-p Ruler.pair
-p Ruler.trio
+processor = Processor.new
+processor.process
