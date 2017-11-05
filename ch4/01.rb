b299eb930253b8bbb6af4be986bbc754a2848a80
yinm <yinm.cminor9@gmail.com>
2017-11-06 07:58:12 +0900

    Add 4-1-6

--- ch4/01.rb
+++ ch4/01.rb
@@ -1,9 +1,16 @@
-class Parent2
+class Parent
   PARENT = 'constant in parent'
 end
 
-class Child < Parent2
+class Child < Parent
 end
 
 p Child.constants
 p Child::PARENT
+
+class GrandChild < Child
+  PARENT = 'constant in grand child'
+end
+
+p GrandChild.constants
+p GrandChild::PARENT
