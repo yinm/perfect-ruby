842272cd56f1fa0539d5e7cbd17d41edc6cebdb0
yinm <yinm.cminor9@gmail.com>
2018-07-14 11:33:03 +0900

    Add commands

--- ch15/todo/lib/todo/command.rb
+++ ch15/todo/lib/todo/command.rb
@@ -1,4 +1,13 @@
+# lib/todo/command.rb
+
 module Todo
   class Command
+    def execute
+      DB.prepare
+    end
+
+    def create_task(name, content)
+      Task.create!(name: name, content: content).reload
+    end
   end
 end
