1fcf57631d1befeed207b11589c7e771dfe35077
yinm <yinm.cminor9@gmail.com>
2018-07-14 12:58:36 +0900

    Add version

--- ch15/todo/lib/todo/command/options.rb
+++ ch15/todo/lib/todo/command/options.rb
@@ -4,7 +4,15 @@ module Todo
   class Command
     module Options
       def self.parse!(argv)
-        {}
+        command_parser = OptionParser.new do |opt|
+          opt.on_head('-v', '--version', 'Show program version') do |v|
+            opt.version = Todo::VERSION
+            puts opt.ver
+            exit
+          end
+        end
+
+        command_parser.parse!(argv)
       end
     end
 
