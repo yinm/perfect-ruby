7f65477404fe459429ee3b4a92f39377106b30dd
yinm <yinm.cminor9@gmail.com>
2018-07-14 13:12:12 +0900

    Add sub command

--- ch15/todo/lib/todo/command/options.rb
+++ ch15/todo/lib/todo/command/options.rb
@@ -4,6 +4,17 @@ module Todo
   class Command
     module Options
       def self.parse!(argv)
+        options = {}
+
+        sub_command_parsers = Hash.new do |k, v|
+          raise ArgumentError, "'#{v}' is not todo sub command."
+        end
+
+        sub_command_parsers['create'] = OptionParser.new do |opt|
+          opt.on('-n VAL', '--name=VAL', 'task name') { |v| options[:name] = v }
+          opt.on('-c VAL', '--content=VAL', 'task content') { |v| options[:content] = v }
+        end
+
         command_parser = OptionParser.new do |opt|
           opt.on_head('-v', '--version', 'Show program version') do |v|
             opt.version = Todo::VERSION
@@ -12,7 +23,15 @@ module Todo
           end
         end
 
-        command_parser.parse!(argv)
+        begin
+          command_parser.order!(argv)
+          options[:command] = argv.shift
+          sub_command_parsers[options[:command]].parse!(argv)
+        rescue OptionParser::MissingArgument, OptionParser::InvalidOption, ArgumentError => e
+          abort e.message
+        end
+
+        options
       end
     end
 
