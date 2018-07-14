730b878425f7f27d4d1e221766b072e2da2dc9d2
yinm <yinm.cminor9@gmail.com>
2018-07-14 11:58:14 +0900

    Add validations

--- ch15/todo/lib/todo/task.rb
+++ ch15/todo/lib/todo/task.rb
@@ -13,5 +13,9 @@ module Todo
       'DONE' => DONE,
       'PENDING' => PENDING
     }.freeze
+
+    validates :name, presence: true, length: { maximum: 140 }
+    validates :content, presence: true
+    validates :status, numericality: true, inclusion: { in: STATUS.values }
   end
 end
