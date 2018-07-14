a4ee905b25d1f806da2726dcf3256cf77888ac96
yinm <yinm.cminor9@gmail.com>
2018-07-14 13:24:14 +0900

    Add other sub commands

--- ch15/todo/lib/todo/command/options.rb
+++ ch15/todo/lib/todo/command/options.rb
@@ -13,6 +13,12 @@ module Todo
           command_parser.order!(argv)
           options[:command] = argv.shift
           sub_command_parsers[options[:command]].parse!(argv)
+
+          if %w(update delete).include?(options[:command])
+            raise ArgumentError, "#{options[:command]} id not found." if argv.empty?
+            options[:id] = Integer(argv.first)
+          end
+
         rescue OptionParser::MissingArgument, OptionParser::InvalidOption, ArgumentError => e
           abort e.message
         end
@@ -28,6 +34,19 @@ module Todo
         sub_command_parsers['create'] = OptionParser.new do |opt|
           opt.on('-n VAL', '--name=VAL', 'task name') { |v| options[:name] = v }
           opt.on('-c VAL', '--content=VAL', 'task content') { |v| options[:content] = v }
+        end
+
+        sub_command_parsers['list'] = OptionParser.new do |opt|
+          opt.on('-s VAL', '--status=VAL', 'list status') { |v| options[:status] = v }
+        end
+
+        sub_command_parsers['update'] = OptionParser.new do |opt|
+          opt.on('-n VAL', '--name=VAL', 'update name') { |v| options[:name] = v }
+          opt.on('-c VAL', '--content=VAL', 'update content') { |v| options[:content] = v }
+          opt.on('-s VAL', '--status=VAL', 'update status') { |v| options[:status] = v }
+        end
+
+        sub_command_parsers['delete'] = OptionParser.new do |opt|
         end
 
         sub_command_parsers
